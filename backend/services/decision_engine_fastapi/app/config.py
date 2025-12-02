7fd57a2ead3d00198a820bd7106b769aa91b6e9e
Beranet-AI <behzadramezan@gmail.com>
2025-12-02 23:28:31 +0330

    Update config.py

--- backend/services/decision_engine_fastapi/app/config.py
+++ backend/services/decision_engine_fastapi/app/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DJANGO_API_BASE_URL: str
-    DJANGO_SERVICE_TOKEN: str
+    DJANGO_API_BASE_URL: str = "http://smartfarm_django:8000/api/v1"
+    DJANGO_SERVICE_TOKEN: str = "super-secret-token"
 
     class Config:
         env_file = ".env.docker"
